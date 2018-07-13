98a445845812a96502e1817178960bc5d1ccfaf5
Cláudio Pereira <git@claudiop.com>
2018-07-13 08:23:15 +0100

    Oops. Bad comparison

--- CLIPy/database/models.py
+++ CLIPy/database/models.py
@@ -114,7 +114,7 @@ class TemporalEntity:
             self.last_year = year
 
     def contains(self, year):
-        return year >= self.first_year and year <= self.first_year
+        return self.first_year <= year <= self.last_year
 
     def __str__(self):
         return ('' if self.first_year is None or self.last_year is None else ' {} - {}'.format(
