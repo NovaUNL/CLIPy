2dd455ceb6e536c44c2124900ab12e5e7bec55a1
Cláudio Pereira <git@claudiop.com>
2020-07-07 10:59:18 +0100

    Update interface accordingly to what is available

--- CLIPy/interface.py
+++ CLIPy/interface.py
@@ -5,6 +5,8 @@ from . import database as db, processors, crawler
 from .session import Session
 from .utils import populate
 
+from .database import models as m
+
 
 class CacheStorage:
     def __init__(self, registry: db.SessionRegistry, database_cache=False):
@@ -28,6 +30,8 @@ class CacheStorage:
 
 
 class Clip:
+    _session = None
+
     def __init__(self, cache: CacheStorage, username, password):
         self.cache: CacheStorage = cache
         self.username = username
@@ -46,41 +50,92 @@ class Clip:
     def fetch_library_group_room_availability(self, date: datetime.date = datetime.now().date()):
         return crawler.crawl_library_group_room_availability(self.session, date)
 
-    def update_admissions(self):
-        processors.institution_task(self.session, self.cache.registry, crawler.crawl_admissions)
+    def list_buildings(self):
+        return list(map(lambda building: building.serialize(), self.cache.controller.get_building_set()))
 
-    def update_teachers(self):
-        processors.department_task(self.session, self.cache.registry, crawler.crawl_teachers)
+    def list_rooms(self):
+        return list(map(lambda building: building.serialize(), self.cache.controller.get_room_set()))
 
-    def update_classes(self):
-        processors.department_task(self.session, self.cache.registry, crawler.crawl_classes)
+    def list_courses(self):
+        return [course.serialize() for course in self.cache.controller.get_courses()]
 
-    def update_class_info(self, year: int, period_part, period_parts):
-        period = self.cache.controller.get_period(period_part, period_parts)
-        if period is None:
-            raise ValueError("Invalid period")
+    def list_students(self):
+        return [student.serialize() for student in self.cache.controller.get_students()]
+
+    def list_teachers(self):
+        return [teacher.serialize() for teacher in self.cache.controller.get_teachers()]
+
+    def get_departments(self):
+        return [department.serialize() for department in self.cache.controller.get_department_set()]
+
+    def get_department(self, id):
+        obj = self.cache.controller.session.query(m.Department).get(id)
+        return None if obj is None else obj.serialize_related()
+
+    def get_class(self, id):
+        obj = self.cache.controller.session.query(m.Class).get(id)
+        return None if obj is None else obj.serialize()
+
+    def get_student(self, id):
+        obj = self.cache.controller.session.query(m.Student).get(id)
+        return None if obj is None else obj.serialize()
+
+    def get_class_instance(self, id):
+        obj = self.cache.controller.session.query(m.ClassInstance).get(id)
+        return None if obj is None else obj.serialize()
+
+    def get_turn(self, id):
+        obj = self.cache.controller.session.query(m.Turn).get(id)
+        return None if obj is None else obj.serialize()
+
+    def get_turn_instance(self, id):
+        obj = self.cache.controller.session.query(m.TurnInstance).get(id)
+        return None if obj is None else obj.serialize()
+
+    def get_enrollment(self, id):
+        obj = self.cache.controller.session.query(m.Enrollment).get(id)
+        return None if obj is None else obj.serialize()
+
+    def get_evaluation(self, id):
+        obj = self.cache.controller.session.query(m.ClassEvaluations).get(id)
+        return None if obj is None else obj.serialize()
+
+    def update_admissions(self, inst_id):
+        processors.institution_task(self.session, self.cache.registry, crawler.crawl_admissions, restriction=inst_id)
+
+    def update_teachers(self, inst_id):
+        processors.department_task(self.session, self.cache.registry, crawler.crawl_teachers, inst_id=inst_id)
+
+    def update_classes(self, inst_id):
+        processors.department_task(self.session, self.cache.registry, crawler.crawl_classes, inst_id=inst_id)
+
+    def update_class_info(self, year: int, period_part: int, period_parts: int):
+        period = self._get_period(period_part, period_parts)
         processors.class_task(self.session, self.cache.registry, crawler.crawl_class_info, year=year, period=period)
 
-    def update_class_enrollments(self, year: int, period_part, period_parts):
-        period = self.cache.controller.get_period(period_part, period_parts)
-        if period is None:
-            raise ValueError("Invalid period")
-        processors.class_task(self.session, self.cache.registry, crawler.crawl_class_info, year=year, period=period)
+    def update_class_enrollments(self, year: int, period_part: int, period_parts: int):
+        period = self._get_period(period_part, period_parts)
+        processors.class_task(self.session, self.cache.registry, crawler.crawl_class_enrollments, year=year,
+                              period=period)
 
-    def update_turns(self, year, period_part, period_parts):
-        period = self.cache.controller.get_period(period_part, period_parts)
-        if period is None:
-            raise ValueError("Invalid period")
+    def update_turns(self, year: int, period_part: int, period_parts: int):
+        period = self._get_period(period_part, period_parts)
         processors.class_task(self.session, self.cache.registry, crawler.crawl_class_turns, year=year, period=period)
 
-    def update_class_files(self, year, period_part, period_parts):
-        period = self.cache.controller.get_period(period_part, period_parts)
-        if period is None:
-            raise ValueError("Invalid period")
+    def update_class_files(self, year: int, period_part: int, period_parts: int):
+        period = self._get_period(period_part, period_parts)
         processors.class_task(self.session, self.cache.registry, crawler.crawl_files, year=year, period=period)
         processors.class_task(self.session, self.cache.registry, crawler.download_files, year=year, period=period)
 
-    @staticmethod
-    def populate(username, password, storage: CacheStorage, year: int = None, period: int = None):
-        populate.bootstrap_database(Session(username, password), storage.registry, year, period)
-        return Clip(storage, username, password)
+    def populate(self, year: int = None, period_part=2, period_parts=None):
+        period = self.cache.controller.get_period(period_part, period_parts)
+        period_id = period.id if period else None
+        populate.bootstrap_database(self.session, self.cache.registry, year, period_id)
+
+    def _get_period(self, period_part, period_parts):
+        if period_part is None or period_parts is None:
+            return None
+        period = self.cache.controller.get_period(period_part, period_parts)
+        if period is None:
+            raise ValueError("Invalid period")
+        return period
--- CLIPy_webservice/views.py
+++ CLIPy_webservice/views.py
@@ -6,7 +6,7 @@ from CLIPy import Clip, CacheStorage
 
 bp = Blueprint('clipy', __name__, url_prefix='')
 
-storage = CacheStorage.postgresql(config.DB_USER, config.DB_PASSWORD, config.DB_NAME)
+storage = CacheStorage.postgresql(config.DB_USER, config.DB_PASSWORD, config.DB_NAME, host=config.DB_HOST)
 clip = Clip(storage, config.CLIP_USER, config.CLIP_PASSWORD)
 
 
@@ -20,9 +20,14 @@ def get_building():
     return jsonify(clip.list_buildings())
 
 
+@bp.route('/rooms/', methods=['GET'])
+def get_rooms():
+    return jsonify(clip.list_rooms())
+
+
 @bp.route('/departments/', methods=['GET'])
 def get_departments():
-    return jsonify(clip.list_departments())
+    return jsonify(clip.get_departments())
 
 
 @bp.route('/courses/', methods=['GET'])
@@ -35,9 +40,49 @@ def get_students():
     return jsonify(clip.list_students())
 
 
-@bp.route('/departments/<int:department_id>', methods=['GET'])
+@bp.route('/teachers/', methods=['GET'])
+def get_teachers():
+    return jsonify(clip.list_teachers())
+
+
+@bp.route('/student/<int:student_id>', methods=['GET'])
+def get_student(student_id):
+    return jsonify(clip.get_student(student_id))
+
+
+@bp.route('/department/<int:department_id>', methods=['GET'])
 def get_department(department_id):
     return jsonify(clip.get_department(department_id))
+
+
+@bp.route('/class/<int:class_id>', methods=['GET'])
+def get_class(class_id):
+    return jsonify(clip.get_class(class_id))
+
+
+@bp.route('/class_inst/<int:instance_id>', methods=['GET'])
+def get_class_instance(instance_id):
+    return jsonify(clip.get_class_instance(instance_id))
+
+
+@bp.route('/turn/<int:turn_id>', methods=['GET'])
+def get_turn(turn_id):
+    return jsonify(clip.get_turn(turn_id))
+
+
+@bp.route('/turn_inst/<int:turn_inst_id>', methods=['GET'])
+def get_turn_instance(turn_inst_id):
+    return jsonify(clip.get_turn_instance(turn_inst_id))
+
+
+@bp.route('/enrollment/<int:enrollment_id>', methods=['GET'])
+def get_enrollment(enrollment_id):
+    return jsonify(clip.get_enrollment(enrollment_id))
+
+
+@bp.route('/evaluation/<int:evaluation_id>', methods=['GET'])
+def get_evaluation(evaluation_id):
+    return jsonify(clip.get_evaluation(evaluation_id))
 
 
 @bp.route('/library_occupation/<string:day>', methods=['GET'])
@@ -47,3 +92,45 @@ def get_library_info(day):
         'individual': clip.fetch_library_individual_room_availability(date),
         'group': clip.fetch_library_group_room_availability(date)
     })
+
+
+@bp.route('/update/classes/<int:institution_id>', methods=['GET'])
+def update_classes(institution_id):
+    clip.update_classes(institution_id)
+    return "Success"
+
+
+@bp.route('/update/teachers/<int:institution_id>', methods=['GET'])
+def update_teachers(institution_id):
+    clip.update_teachers(institution_id)
+    return "Success"
+
+
+@bp.route('/update/admissions/<int:institution_id>', methods=['GET'])
+def update_admissions(institution_id):
+    clip.update_admissions(institution_id)
+    return "Success"
+
+
+@bp.route('/update/class_info/<int:year>/<int:year_parts>/<int:part>', methods=['GET'])
+def update_class_info(year, year_parts, part):
+    clip.update_class_info(year, year_parts, part)
+    return "Success"
+
+
+@bp.route('/update/class_enrollments/<int:year>/<int:year_parts>/<int:part>', methods=['GET'])
+def update_class_enrollments(year, year_parts, part):
+    clip.update_class_enrollments(year, year_parts, part)
+    return "Success"
+
+
+@bp.route('/update/class_files/<int:year>/<int:year_parts>/<int:part>', methods=['GET'])
+def update_class_files(year, year_parts, part):
+    clip.update_class_files(year, year_parts, part)
+    return "Success"
+
+
+@bp.route('/update/turns/<int:year>/<int:year_parts>/<int:part>', methods=['GET'])
+def update_turns(year, year_parts, part):
+    clip.update_turns(year, year_parts, part)
+    return "Success"
