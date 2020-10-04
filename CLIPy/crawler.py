dd927e71430703c6788d3eb0adb3056803def39a
Cláudio Pereira <git@claudiop.com>
2020-10-04 19:04:09 +0100

    Merge the populate script with the other crawlers and interface.

--- CLIPy/crawler.py
+++ CLIPy/crawler.py
@@ -66,6 +66,107 @@ class PageCrawler(Thread):
         self.db_registry.remove()
 
 
+def crawl_departments(session: WebSession, database: db.Controller):
+    """
+    Finds new departments and adds them to the database. *NOT* thread-safe.
+
+    :param session: Web session
+    :param database: Database controller
+    """
+    found = {}  # id -> Department
+
+    # Find the departments which existed each year
+    for year in range(INSTITUTION_FIRST_YEAR, datetime.now().year + 2):
+        log.info(f"Crawling departments of institution. Year:{year}")
+        hierarchy = session.get_simplified_soup(urls.DEPARTMENTS.format(institution=INSTITUTION_ID, year=year))
+        for department_id, name in parser.get_departments(hierarchy):
+            if department_id in found:  # update creation year
+                found[department_id].add_year(year)
+            else:  # insert new
+                department = db.candidates.Department(department_id, name, year, year)
+                found[department_id] = department
+    database.add_departments(found.values())
+
+
+def crawl_buildings(session: WebSession, database: db.Controller):
+    """
+    Finds new buildings and adds them to the database. *NOT* thread-safe.
+
+    :param session: Web session
+    :param database: Database controller
+    """
+
+    buildings = {}  # id -> Candidate
+    for year in range(INSTITUTION_FIRST_YEAR, datetime.now().year + 1):
+        for period in database.get_period_set():
+            log.info(f"Crawling buildings. Year:{year}. Period: {period}")
+            page = session.get_simplified_soup(
+                urls.BUILDINGS.format(
+                    institution=INSTITUTION_ID,
+                    year=year,
+                    period=period.part,
+                    period_type=period.letter))
+            page_buildings = parser.get_buildings(page)
+            for identifier, name in page_buildings:
+                candidate = db.candidates.Building(identifier=identifier, name=name, first_year=year, last_year=year)
+                if identifier in buildings:
+                    other = buildings[identifier]
+                    if other != candidate:
+                        raise Exception("Found two different buildings going by the same ID")
+                    other.add_year(year)
+                else:
+                    buildings[identifier] = candidate
+
+    for building in buildings.values():
+        log.debug(f"Adding building {building} to the database.")
+        database.add_building(building)
+
+
+def crawl_courses(session: WebSession, database: db.Controller):
+    """
+    Finds new courses and adds them to the database. *NOT* thread-safe.
+
+    :param session: Web session
+    :param database: Database controller
+    """
+    courses = {}  # identifier -> Candidate pairs
+
+    # Obtain course id-name pairs from the course list page
+    page = session.get_simplified_soup(urls.COURSES.format(institution=INSTITUTION_ID))
+    for identifier, name in parser.get_course_names(page):
+        # Fetch the course curricular plan to find the activity years
+        page = session.get_simplified_soup(urls.CURRICULAR_PLANS.format(institution=INSTITUTION_ID, course=identifier))
+        first, last = parser.get_course_activity_years(page)
+        candidate = db.candidates.Course(identifier, name, first_year=first, last_year=last)
+        courses[identifier] = candidate
+
+    # fetch course abbreviation from the statistics page
+    integrated_master_degree = list(filter(lambda deg: deg.id == 4, database.get_degree_set()))[0]
+    for degree in database.get_degree_set():
+        if degree.id == 4:  # Skip integrated masters
+            continue
+        page = session.get_simplified_soup(urls.STATISTICS.format(institution=INSTITUTION_ID, degree=degree.iid))
+        for identifier, abbreviation in parser.get_course_abbreviations(page):
+            if identifier in courses:
+                course = courses[identifier]
+                courses[identifier].abbreviation = abbreviation
+                if degree.id == 2 and abbreviation.startswith('MI'):  # Distinguish masters from integrated masters
+                    course_page = session.get_simplified_soup(
+                        urls.COURSES.format(institution=INSTITUTION_ID, course=course.id))
+                    if course_page.find(text=re.compile(".*Mestrado Integrado.*")):
+                        courses[identifier].degree = integrated_master_degree
+                    else:
+                        courses[identifier].degree = degree
+                else:
+                    courses[identifier].degree = degree
+            else:
+                raise Exception(
+                    "{}({}) was listed in the abbreviation list but a corresponding course wasn't found".format(
+                        abbreviation, identifier))
+
+    database.add_courses(courses.values())
+
+
 def crawl_rooms(session: WebSession, database: db.Controller, building: db.models.Building):
     building = database.session.merge(building)
     rooms = {}  # id -> Candidate
--- CLIPy/interface.py
+++ CLIPy/interface.py
@@ -133,12 +133,74 @@ class Clip:
         processors.class_task(self.session, self.cache.registry, crawler.crawl_files, year=year, period=period)
         processors.class_task(self.session, self.cache.registry, crawler.download_files, year=year, period=period)
 
-    def populate(self, year: int = None, period_part=2, period_parts=None):
+    def bootstrap_database(self, year: int = None, period_part=2, period_parts=None):
+        """
+        | Bootstraps a database from scratch.
+        | Can also be used as an updater but would be a waste of resources in most scenarios.
+        | This is a very everything-intensive task. It uses a lot of CPU, DB IO and if I had to guess it also heats
+          up the server by a few degrees.
+
+        :param year: (Optional) Year filter
+        :param period_part: (Optional) Period filter (part of period_parts)
+        :param period_parts: (Optional) Period filter
+        """
+
         if period_part is not None and period_parts is not None:
             period = self.cache.controller.get_period(period_part, period_parts)
         else:
             period = None
-        populate.bootstrap_database(self.session, self.cache.registry, year, period)
+
+        main_thread_db_controller = db.Controller(self.cache.registry, cache=False)
+
+        # Find departments.
+        crawler.crawl_departments(self.session, main_thread_db_controller)
+
+        # Find buildings (depends on up-to-date departments).
+        crawler.crawl_buildings(self.session, main_thread_db_controller)
+
+        # Find rooms (depends on up-to-date institutions and buildings).
+        processors.building_task(self.session, self.cache.registry, crawler.crawl_rooms)
+
+        # Find classes (depends on up-to-date departments).
+        processors.department_task(self.session, self.cache.registry, crawler.crawl_classes)
+
+        # Find courses.
+        crawler.crawl_courses(self.session, main_thread_db_controller)
+
+        # Looks up the national access contest admission tables looking for students current statuses.
+        # Depends on up-to-date institutions.
+        processors.year_task(
+            self.session,
+            self.cache.registry,
+            crawler.crawl_admissions,
+            from_year=INSTITUTION_FIRST_YEAR,
+            to_year=INSTITUTION_LAST_YEAR)
+
+        # Finds student enrollments to class instances.
+        processors.class_task(
+            self.session,
+            self.cache.registry,
+            crawler.crawl_class_enrollments,
+            year=year,
+            period=period)
+
+        # Find class information such as objectives
+        processors.class_task(self.session, self.cache.registry, crawler.crawl_class_info, year=year, period=period)
+
+        # Finds class instance turns and updates their data if needed.
+        processors.class_task(self.session, self.cache.registry, crawler.crawl_class_turns, year=year, period=period)
+
+        # Find teachers (depends on up-to-date departments and turns).
+        processors.department_task(self.session, self.cache.registry, crawler.crawl_teachers)
+
+        # Finds uploaded file listings for every class
+        processors.class_task(self.session, self.cache.registry, crawler.crawl_files, year=year, period=period)
+
+        # Downloads known files
+        processors.class_task(self.session, self.cache.registry, crawler.download_files, year=year, period=period)
+
+        # Finds class instance grades
+        processors.class_task(self.session, self.cache.registry, crawler.crawl_grades, year=year, period=period)
 
     def _get_period(self, period_part, period_parts):
         if period_part is None or period_parts is None:
